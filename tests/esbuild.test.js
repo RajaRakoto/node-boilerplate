df646b87e94cb96be65866dddbe1c3ca70ffeaea
raja <raja.rakoto7@gmail.com>
2024-01-15 10:27:27 +0300

    tests: add esbuild test

---
+++ tests/esbuild.test.js
@@ -0,0 +1,13 @@
+import * as esbuild from 'esbuild';
+
+esbuild
+  .build({
+    entryPoints: ['src/main.ts'],
+    bundle: true,
+    minify: true,
+    sourcemap: false,
+    target: ['es2022'],
+    outfile: 'tests/bundle.min.test.cjs',
+    platform: 'node',
+  })
+  .catch(() => process.exit(1));
