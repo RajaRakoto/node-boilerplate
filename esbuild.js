bc92ce37160e19323b46f691f52d45cf7afdf5b0
raja <raja.rakoto7@gmail.com>
2024-03-26 07:44:25 +0300

    update esbuild

--- esbuild.js
+++ esbuild.js
@@ -1,13 +1,13 @@
-import * as esbuild from 'esbuild';
+import * as esbuild from "esbuild";
 
 esbuild
-  .build({
-    entryPoints: ['./src/index.ts'],
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    target: ['es2022'],
-    outfile: './dist/bundle.min.cjs',
-    platform: 'node',
-  })
-  .catch(() => process.exit(1));
+	.build({
+		entryPoints: ["./src/index.ts"],
+		bundle: true,
+		minify: true,
+		sourcemap: true,
+		target: ["ESNext"],
+		outfile: "./dist/bundle.cjs",
+		platform: "node",
+	})
+	.catch(() => process.exit(1));
